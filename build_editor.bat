7104d2cc1f30c0a5fd24d951f357a957c7bcf79b
JayM3 <jayem.malundo@gmail.com>
2026-09-18 13:56:44 +0200

    Untrack drafts, database editor, and unused data files

--- build_editor.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Building AMT Database Editor...
-python -m PyInstaller --onefile --windowed --name "AMT_Database_Editor" --distpath "." --workpath "build_tmp" --specpath "build_tmp" db_editor.py
-if exist build_tmp rmdir /s /q build_tmp
-echo.
-echo Done! AMT_Database_Editor.exe is ready.
-pause
